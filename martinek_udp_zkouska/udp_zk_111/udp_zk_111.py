ee4fe11e34c2878fed15d4d88b9b09b6264983d8
martineda96 <93780022+martineda96@users.noreply.github.com>
2022-02-13 17:49:25 +0100

    Add files via upload

---
+++ martinek_udp_zkouska/udp_zk_111/udp_zk_111.py
@@ -0,0 +1,47 @@
+#Eight queens solution
+#David Martínek, 2022
+
+#Function searching for queens in row, column and diagonals
+def LineChecker(row,column):
+
+    for i in range(QUEENS_NUM):
+        if board[row][i] == "Q":
+            return False
+
+    for i in range(QUEENS_NUM):
+        if board[i][column] == "Q":
+            return False
+
+    for i in range(QUEENS_NUM):
+        for j in range(QUEENS_NUM):
+            if board[i][j] == "Q":
+                if abs(i - row) == abs(j - column):
+                    return False
+    return True
+
+#Fuction placing queens on board using backtracking
+def Placement(column):
+
+    if column >= QUEENS_NUM:
+        return True
+
+    for i in range(QUEENS_NUM):
+                if LineChecker(i, column):
+                    board[i][column] = "Q"
+                    if Placement(column + 1) == True:
+                        return True
+                    board[i][column] = "-"
+    return board
+
+#Initial variables and creation of board [8x8]
+QUEENS_NUM = 8
+board = [["-"]*QUEENS_NUM for i in range(QUEENS_NUM)]
+
+#Placement
+Placement(0)
+
+#Board print
+for i in range(QUEENS_NUM):
+    for j in range(QUEENS_NUM):
+        print(board[i][j], end=" ")
+    print("")
---
+++ martinek_udp_zkouska/udp_zk_30/udp_zk_30.py
@@ -0,0 +1,50 @@
+#Simpson's approximation of integral
+#David Martínek, 2022
+
+from math import e
+
+#Function that is solved (e**x can be substituted with any other continuous and differentiable function)
+def F(x):
+    value = float(e**x)
+    return value
+
+#User-selected inputs
+try:
+    a = int(input("Zvolte dolní mez integrálu a:"))
+    b = int(input("Zvolte horní mez integrálu b:"))
+    if a > b:
+        c = a
+        d = b
+        a = d
+        b = c
+
+    n = int(input("Zvolte počet podintervalů n:"))
+    while n <= 0:
+        n = int(input("Parametr n musí být kladný. Zadejte novou hodnotu:"))
+        
+except ValueError:
+    print("Hodnoty musí být zadány v podobě celého čísla.")
+
+#Inicial variables and calculation of h from inputs
+sum = 0
+h = (b - a)/n
+
+#Cycle used to calculate and sumarize every subinterval of function within borders
+for i in range(n+1):
+    if i % 2 == 0:
+        k = 4
+    else:
+        k = 2
+    if i == 0 or i == n:
+        k = 1
+    x = h*(i)
+    step_i_result= k*F(x)
+    sum += step_i_result
+
+#Correction for inverted inputs
+if c > d:
+    h = -h
+
+#Presenting the result of an approximation
+result = (h/3)*sum
+print(result)
